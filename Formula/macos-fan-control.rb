035b8ff28aef75a28faa309ea11e4f4ad44d9ccc
Marco Cestari <125188019+marcocestari-tn@users.noreply.github.com>
2026-08-09 14:48:09 +0200

    macos-fan-control 0.3.1
    Closes #4.
    
    Signed-off-by: Marco Cestari <125188019+marcocestari-tn@users.noreply.github.com>

--- Formula/macos-fan-control.rb
+++ Formula/macos-fan-control.rb
@@ -1,8 +1,8 @@
 class MacosFanControl < Formula
   desc "Experimental Apple Silicon fan controller for macOS"
   homepage "https://github.com/marcocestari-tn/macos-fan-control"
-  url "https://github.com/marcocestari-tn/macos-fan-control/archive/refs/tags/v0.3.0.tar.gz"
-  sha256 "cc19c1a5792c556cc7161f1ddb9013a08eb386f5bfa2463156051e6f8c06f033"
+  url "https://github.com/marcocestari-tn/macos-fan-control/archive/refs/tags/v0.3.1.tar.gz"
+  sha256 "4d2761ad90cabe83fe7c075f71755f5e4752731c7e800846d211f51e9f85b367"
   license "MIT"
 
   depends_on arch: :arm64
@@ -26,7 +26,7 @@ class MacosFanControl < Formula
   end
 
   test do
-    assert_match "fan-control 0.3.0", shell_output("#{bin}/fan-control --version")
+    assert_match "fan-control 0.3.1", shell_output("#{bin}/fan-control --version")
     assert_match "self-test: OK", shell_output("#{bin}/fan-control --self-test")
   end
 end
