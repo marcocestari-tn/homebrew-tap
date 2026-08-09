d72b2ce52e4d16ceb01f7ae5beca0b0c2dec62e4
Marco Cestari <125188019+marcocestari-tn@users.noreply.github.com>
2026-08-09 13:40:50 +0000

    macos-fan-control: add 0.4.0 bottle.

--- Formula/macos-fan-control.rb
+++ Formula/macos-fan-control.rb
@@ -5,6 +5,11 @@ class MacosFanControl < Formula
   sha256 "96d748233497bc0b824e53d1aea9cf8913eff2d84bde8a70dc4c4f6d611c2d79"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/marcocestari-tn/homebrew-tap/releases/download/macos-fan-control-0.4.0"
+    sha256 arm64_tahoe: "bc1fa8feae8a8d341034c64aea5784b40baa37e1ac3b49b6d97f6e99d18e7fa1"
+  end
+
   depends_on arch: :arm64
   depends_on "macmon"
   depends_on macos: :ventura
