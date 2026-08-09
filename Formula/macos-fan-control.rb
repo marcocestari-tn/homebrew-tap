03e192254e84a171ba5503217337c8297b5147a5
Marco Cestari <125188019+marcocestari-tn@users.noreply.github.com>
2026-08-09 16:42:02 +0200

    macos-fan-control 0.4.2
    Closes #9.
    
    Signed-off-by: Marco Cestari <125188019+marcocestari-tn@users.noreply.github.com>

--- Formula/macos-fan-control.rb
+++ Formula/macos-fan-control.rb
@@ -1,14 +1,9 @@
 class MacosFanControl < Formula
   desc "Experimental Apple Silicon fan controller for macOS"
   homepage "https://github.com/marcocestari-tn/macos-fan-control"
-  url "https://github.com/marcocestari-tn/macos-fan-control/archive/refs/tags/v0.4.1.tar.gz"
-  sha256 "9469bfe60007bc47f08a0b9c4b7ef82708a487434162d5014bc5b53cf12bf35c"
+  url "https://github.com/marcocestari-tn/macos-fan-control/archive/refs/tags/v0.4.2.tar.gz"
+  sha256 "1ac25674ea2b8a7bc924985c93e6fec7fd8aee0740c2544a1bd27aeee85f7f40"
   license "MIT"
-
-  bottle do
-    root_url "https://github.com/marcocestari-tn/homebrew-tap/releases/download/macos-fan-control-0.4.1"
-    sha256 arm64_tahoe: "efd4e5658c2a85af080f3da2eb8fc68621f57143ee8af459969273028ee5cf6d"
-  end
 
   depends_on arch: :arm64
   depends_on "macmon"
